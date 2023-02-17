7a5147a4460469b0bfbafb6df39cfb8838568431
Victoriaaston <Aston.Victoria27@gmail.com>
2023-02-16 19:46:52 -0600

    update names

--- seed.js
+++ seed.js
@@ -7,20 +7,20 @@ const Item = require('./models/item');
 
   await Item.deleteMany({});
   const items = await Item.create([
-    {photo: 'https://iili.io/HE12eUu.jpg', name: 'Tie-Dye', size: '14x8', price: 21.99},
+    {photo: 'https://iili.io/HE12eUu.jpg', name: 'Vibrant Swirl', size: '14x8', price: 21.99},
     {photo: 'https://iili.io/HE12DOv.jpg', name: 'Skull', size: '14x8', price: 38.99},
-    {photo: 'https://iili.io/HE13mYu.jpg', name: 'light-outfit', size: '4x4', price: 12.99},
-    {photo: 'https://iili.io/HE1FP8g.jpg', name: 'dark-outfit', size: '4x4', price: 12.99},
-    {photo: 'https://iili.io/HE1FyFt.jpg', name: 'smoke-profile', size: '4x4', price: 13.99},
-    {photo: 'https://iili.io/HE1KIHu.jpg', name: 'diamonds', size: '14x8', price: 25.99},
-    {photo: 'https://iili.io/HE1KXxR.jpg', name: 'red-rose-newspaper', size: '10x6', price: 22.99},
-    {photo: 'https://iili.io/HE1K6s2.jpg', name: 'yellow-rose-newspaper', size: '10x6', price: 22.99},
-    {photo: 'https://iili.io/HE1Kt5u.jpg', name: 'hands', size: '10x6', price: 18.99},
-    {photo: 'https://iili.io/HE1fJqB.jpg', name: 'coaster', size: '3x3', price: 4.95},
+    {photo: 'https://iili.io/HE13mYu.jpg', name: 'Summer Sketch', size: '4x4', price: 12.99},
+    {photo: 'https://iili.io/HE1FP8g.jpg', name: 'Fall Sketch', size: '4x4', price: 12.99},
+    {photo: 'https://iili.io/HE1FyFt.jpg', name: 'Winter Sketch', size: '4x4', price: 13.99},
+    {photo: 'https://iili.io/HE1KIHu.jpg', name: 'Diamonds', size: '14x8', price: 25.99},
+    {photo: 'https://iili.io/HE1KXxR.jpg', name: 'Vintage Newspaper', size: '10x6', price: 22.99},
+    {photo: 'https://iili.io/HE1K6s2.jpg', name: 'Vintage Newspaper', size: '10x6', price: 22.99},
+    {photo: 'https://iili.io/HE1Kt5u.jpg', name: 'Reach', size: '10x6', price: 18.99},
+    {photo: 'https://iili.io/HE1fJqB.jpg', name: 'Vibrant Swirl Coaster', size: '3x3', price: 4.95},
     {photo: 'https://iili.io/HE1fFdF.jpg', name: 'Skeleton', size: '24x12', price: 321.99},
-    {photo: 'https://iili.io/HE1fosp.jpg', name: 'light-blue-record', size: '12x12', price: 38.99},
-    {photo: 'https://iili.io/HE1fE1S.jpg', name: 'smokey-record', size: '12x12', price: 38.99},
-    {photo: 'https://iili.io/HE1fhmb.jpg', name: 'dark-blue-record', size: '12x12', price: 38.99},
+    {photo: 'https://iili.io/HE1fosp.jpg', name: 'Summer Record', size: '12x12', price: 38.99},
+    {photo: 'https://iili.io/HE1fE1S.jpg', name: 'Smokey Record', size: '12x12', price: 38.99},
+    {photo: 'https://iili.io/HE1fhmb.jpg', name: 'Waves Record', size: '12x12', price: 38.99},
   ]);
 
   console.log(items)
